f798ffb55a77397e7d133100a43197f13285f7ee
PavolKohar <koharpavol98@gmail.com>
2025-09-12 10:16:57 +0200

    Created security Filter chain

--- src/main/java/com/palci/CoachProgram/Configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/com/palci/CoachProgram/Configuration/ApplicationSecurityConfiguration.java
@@ -2,10 +2,40 @@ package com.palci.CoachProgram.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ApplicationSecurityConfiguration {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http
+                .authorizeHttpRequests(auth->auth
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form->form
+                        .loginPage("/login") // Login page
+                        .loginProcessingUrl("/login") // Post method
+                        .defaultSuccessUrl("/",true) // Redirect
+                        .usernameParameter("email")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/account/login?logout")
+                        .permitAll()
+                );
 
+        return http.build();
 
+    }
+
+    // Hash management
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
