2a4c5229fa7164a5bd5a64586042279459b6c244
PavolKohar <koharpavol98@gmail.com>
2025-09-12 10:56:58 +0200

    Secured ClientController just for login users

--- src/main/java/com/palci/CoachProgram/Controllers/ClientController.java
+++ src/main/java/com/palci/CoachProgram/Controllers/ClientController.java
@@ -1,11 +1,13 @@
 package com.palci.CoachProgram.Controllers;
 
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Secured({"ROLE_ADMIN", "ROLE_USER"})
 @RequestMapping("/clients")
 public class ClientController {
 
