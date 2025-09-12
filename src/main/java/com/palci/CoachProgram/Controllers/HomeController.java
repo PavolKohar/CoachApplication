53a923610895e23f1b1b9b2e0936980d4e46e077
PavolKohar <koharpavol98@gmail.com>
2025-09-12 09:05:07 +0200

    Created controller for home page

---
+++ src/main/java/com/palci/CoachProgram/Controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.palci.CoachProgram.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+    @GetMapping
+    public String renderHomePage(){
+        return "pages/home/index";
+    }
+}
